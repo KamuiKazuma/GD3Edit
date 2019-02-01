094369ce53dc045a68e4eb43d476475132cbea59
Lisa A. Murray <43791059+KamuiKazuma@users.noreply.github.com>
2019-02-01 11:09:38 -0600

    Updated module compiler script

--- compile-module.bat
+++ compile-module.bat
@@ -5,12 +5,18 @@ REM 0 = No Error
 REM 1 = Source file not found
 REM 2 = Output file not found
 
-ECHO compile-module.bat
+REM Syntax:
+REM compile-module[.bat] <source file> <output file> <output folder>
+REM <source file> - The file to compile
+REM <output file> - The output (*.a) file
+
+ECHO.
+ECHO %~0
 ECHO FreeBASIC Static Library Compiler Tool
-ECHO v1.2.0
+ECHO v1.3
 ECHO.
 
-REM compile module if it exists
+REM compile library source file if it exists
 IF EXIST %~f1 (
 	IF DEFINED FBDEBUG (
 		fbc -g -lib %~f1 -x %~f2
@@ -20,14 +26,33 @@ IF EXIST %~f1 (
 	SET ERRORLEVEL=0
 ) ELSE (
 	SET ERRORLEVEL=1
+	GOTO ERR
+)
+
+REM Check for output file
+IF EXIST %~f2 (
+	SET ERRORLEVEL=0
+) ELSE (
+	SET ERRORLEVEL=1
+	GOTO ERR
+)
+
+REM Error handler
+:ERR
+IF NOT ERRORLEVEL 0 (
+	GOTO ERR%ERRORLEVEL%
+) ELSE (
 	GOTO EOF
 )
 
-IF EXIST %~f2 (
-	MOVE %~f2 %3
-	SET ERRORLEVEL=0
-) ELSE (
-	SET ERRORLEVEL=2
-)
+:ERR1
+ECHO %~0 ERROR: Source file %~f1 not found.
+GOTO EOF
+
+:ERR2
+ECHO.
+ECHO %~0 ERROR: Output file %~f2 not found.
+ECHO.
 
 :EOF
+EXIT
