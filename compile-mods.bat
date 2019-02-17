4659f6b238df717a68f995a6a69034b87e5d520a
KamuiKazuma <k4mu1kazum4@gmail.com>
2019-02-17 17:55:28 -0600

    updated compile script

--- compile-mods.bat
+++ compile-mods.bat
@@ -2,6 +2,7 @@
 
 IF EXIST ".\compile-mod.bat" (
 	CALL ".\compile-mod.bat" ".\Mod\filenameinfo.bas" ".\Mod\filenameinfo.o" ".\filenameinfo.o"
+	CALL ".\compile-mod.bat" ".\Mod\vgmhead.bas" ".\Mod\vgmhead.o" ".\vgmhead.o"
 	GOTO EOF
 ) ELSE (
 	SET ERRORLEVEL=3
